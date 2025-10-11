f948e8cc04dca55a32ba6538a1931f64e3a7eeac
MariaLuizaMB <mariamadeirabm2@gmail.com>
2025-10-10 21:06:34 -0300

    Update Files

--- Implementacao_MLP/main.py
+++ Implementacao_MLP/main.py
@@ -6,14 +6,11 @@ from KnnSkl import run_knn_sklearn
 from MLP_Classifier import run_mlp_on_dataset
 
 if __name__ == "__main__":
-    # Rodar MLP
     run_mlp_on_dataset("Iris", load_iris().data, load_iris().target)
     run_mlp_on_dataset("Wine", load_wine().data, load_wine().target)
 
-    # Rodar KNN Hardcore
     hc_results_iris = run_knn_hardcore("Iris", load_iris())
     hc_results_wine = run_knn_hardcore("Wine", load_wine())
 
-    # Rodar KNN Sklearn (precisa ajustar igual hardcore)
     sk_results_iris = run_knn_sklearn("Iris", load_iris())
     sk_results_wine = run_knn_sklearn("Wine", load_wine())
